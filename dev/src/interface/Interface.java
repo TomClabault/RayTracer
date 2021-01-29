96459a0adf38b3fcc43297cdc619aeb8e24a5266
DRUM0ND <61603811+DRUM0ND@users.noreply.github.com>
2021-01-29 11:47:42 +0100

    Creation du package Interface

---
+++ dev/src/interface/Interface.java
@@ -0,0 +1,10 @@
+package interface;
+
+/*import javafx.application.Application;*/
+
+public class Interface {
+
+    public static void main(String[] args) {
+        System.out.println("toto");
+    }
+}
