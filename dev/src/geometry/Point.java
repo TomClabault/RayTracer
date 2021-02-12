559323f1c6411e4cce5694a948e4bfda9fec4481
Willie Tanzil <willie.tanzil99@gmail.com>
2021-02-12 10:38:25 +0100

    point class

--- dev/src/geometry/Point.java
+++ dev/src/geometry/Point.java
@@ -2,5 +2,37 @@ package geometry;
 
 public class Point 
 {
+    private float x,y,z;
+
+    public Point(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float getX(){
+        return this.x;
+    }
+
+    public float getY(){
+        return this.y;
+    }
+
+    public float getZ(){
+        return this.z;
+    }
+
+    public void setX(float newX){
+        this.x = newX;
+    }
+
+    public void setY(float newY){
+        this.y = newY;
+    }
+
+    public void setZ(float newZ){
+        this.z = newZ;
+    }
+
 
 }
