4a8c80e00213d1a0d8dace5a6cde9d65c22efc78
Willie Tanzil <willie.tanzil99@gmail.com>
2021-02-14 15:35:28 +0100

    methode addTriangle et removeTriangle dans la classe Shape

--- dev/src/geometry/Shape.java
+++ dev/src/geometry/Shape.java
@@ -14,20 +14,25 @@ public class Shape
     }
 
     /*addTriangle va ajouter un triangle, faut juste ajouter les points du triangle qu'on veut ajouter*/
-    public ArrayList<Triangle> addTriangle(Point a,Point b,Point c)
+    public void addTriangle(Point a,Point b,Point c)
     {
         Triangle truc = new Triangle(a,b,c);
         this.forme.add(truc);
-        return this.forme;
     }
 
     /*removeTriangle va supprimer un triangle. elle va chercher le triangle avec les points saisis dans le parametre, puis elle le supprime*/
-    public ArrayList<Triangle> removeTriangle(Point a, Point b, Point c)
+    public void removeTriangle(Point a, Point b, Point c)
     {
         Triangle truc = new Triangle(a,b,c);
-        int toremove = this.forme.indexOf(truc);
-        this.forme.remove(toremove);
-        return this.forme;
+        if (this.forme.indexOf(truc) >= 0)
+        {
+            int toremove = this.forme.indexOf(truc);
+            this.forme.remove(toremove);
+        }
+        else
+        {
+            System.out.println("le triangle que vous souhaitez supprimer n'existe pas");
+        }
     }
 
 }
