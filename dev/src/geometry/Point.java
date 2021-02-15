843fbe885856480c72ef1eb8481f69946f6da9a2
Tom Clabault <Tomikaddle@orange.fr>
2021-02-15 18:24:37 +0100

    Ajout d'une méthode à Point pour soustraire deux points

--- dev/src/geometry/Point.java
+++ dev/src/geometry/Point.java
@@ -4,7 +4,8 @@ public class Point
 {
     private float x,y,z;
 
-    public Point(float x, float y, float z){
+    public Point(float x, float y, float z)
+    {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -34,5 +35,16 @@ public class Point
         this.z = newZ;
     }
 
-
+    /*
+     * Soustrait deux points coordonnées à coordonnées et retourne le point résultant
+     * 
+     * @param a Le premier point de coordoonées (a1, a2, a3)
+     * @param b Le deuxième point de coordonnées (b1, b2, b3)
+     * 
+     * @return Le point de coordonnées (a1 - b1, a2 - b2, a3 - b3)
+     */
+    public static Point sub(Point a, Point b)
+    {
+    	return new Point(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
 }
