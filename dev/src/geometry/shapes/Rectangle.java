bfb158b7adf5a0d0a62adf983acd765566a5a5a5
Willie Tanzil <willie.tanzil99@gmail.com>
2021-02-16 20:56:14 +0100

    Classe Rectangle (beta)

--- dev/src/geometry/shapes/Rectangle.java
+++ dev/src/geometry/shapes/Rectangle.java
@@ -1,8 +1,56 @@
 package geometry.shapes;
 
 import geometry.shapes.Triangle;
+import geometry.Shape;
+import geometry.Point;
+import java.util.ArrayList;
+import java.lang.Math;
+
 
 public class Rectangle implements Shape
 {
-	ArrayList<Triangle> 
+
+	protected double height,width,length,volume;
+	protected float diagonal;
+	protected Point coin1,coin2,difference;
+	protected ArrayList<Triangle> listeTriangle;
+
+	public Rectangle(Point coin1, float height, float length, float width)
+	{	/*ici coin1 peut etre considere comme le point de depart*/
+		this.coin1 = coin1;
+		this.height = height;
+		this.length = length;
+		this.width = width;
+	}
+
+	public Rectangle(Point coin1, Point coin2)
+	{
+		this.coin1 = coin1;
+		this.coin2 = coin2;
+	}
+
+
+
+	public void buildRectangle()
+	{
+		if(this.coin1 != null && this.coin2 != null)
+		{
+			/*si les longeurs sont inconnus*/
+			/*this.difference = this.coin2 - this.coin1;*/
+			this.length = this.coin2.getX() - this.coin1.getX();
+			this.height = this.coin2.getZ() - this.coin1.getZ();
+			this.width = this.coin2.getY() - this.coin1.getY();
+		}
+		else if(this.coin1 != null && this.length == length  && this.height == height && this.width == width)
+		{
+			/*si le coin est inconnu*/
+			this.diagonal = (float) Math.sqrt(Math.pow(this.length, 2) + Math.pow(this.height, 2) + Math.pow(this.width, 2));
+			this.coin2.setX(this.coin1.getX() + this.diagonal);
+			this.coin2.setY(this.coin1.getY() + this.diagonal);
+			this.coin2.setY(this.coin1.getZ() + this.diagonal);
+			this.volume = this.length * this.height * this.width;
+		}
+
+	}
+
 }
