b2c86dc42cd81f5ad70a02c5338b9f58c8d7160f
Tom Clabault <Tomikaddle@orange.fr>
2021-02-17 18:39:01 +0100

    Légère modification de la signature de la méthode computeImage de RayTracer

--- dev/src/rayTracer/RayTracer.java
+++ dev/src/rayTracer/RayTracer.java
@@ -1,6 +1,7 @@
 package rayTracer;
 
 import javafx.scene.paint.Color;
+import scene.Scene;
 
 /*
  * Une instance de RayTracer créée à partir de la largeur et de la hauteur du rendu voulu. Permet de générer les 
@@ -22,7 +23,14 @@ public class RayTracer
 		renderedPixels = new Color[height][length];
 	}
 	
-	public Color[][] computeImage(Camera camera, Light lightSource)
+	/*
+	 * Calcule tous les pixels de la scène donnée en argument et retourne un tableau de couleur RGB correspondant aux pixels
+	 * 
+	 * @param renderScene La scène de rendu contenant les informations pour rendre l'image
+	 * 
+	 * @return Un tableau de Color.RGB(r, g, b) de dimension renderHeight*renderLength
+	 */
+	public Color[][] computeImage(Scene renderScene)
 	{
 		return renderedPixels;
 	}
