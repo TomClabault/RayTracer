73230b1a65f3583e398bfcf9c98ff93bb67418cb
Tom Clabault <Tomikaddle@orange.fr>
2021-02-17 18:32:53 +0100

    Création de la classe Scene

---
+++ dev/src/scene/Scene.java
@@ -0,0 +1,31 @@
+package scene;
+
+import java.util.ArrayList;
+
+import geometry.Shape;
+import scene.lights.Light;
+
+/*
+ * Permet de représenter une scène de rendu contenant une caméra, une lumière et une liste de formes
+ */
+public class Scene 
+{
+	Camera camera;
+	Light light;
+	
+	ArrayList<Shape> shapes;
+	
+	/*
+	 * Crée la scène à partir d'une caméra, d'une lumière et d'une liste de forme
+	 * 
+	 * @param camera La camera de la scène à travers laquelle le rendu sera fait
+	 * @param light La lumière permettant d'illuminer la scène
+	 * @param shapes Liste de forme qui seront rendues
+	 */
+	public Scene(Camera camera, Light light, ArrayList<Shape> shapes)
+	{
+		this.camera = camera;
+		this.light = light;
+		this.shapes = shapes;
+	}
+}
