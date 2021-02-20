91f902c394259f75ff29ea91f5dde579f90d9a70
Tom Clabault <Tomikaddle@orange.fr>
2021-02-20 12:01:36 +0100

    Suppression d'un test dans SphereTests qui ne marchait pas à cause de la précision de la virgule flottante

--- dev/src/tests/IntersectionSphere.java
+++ dev/src/tests/IntersectionSphere.java
@@ -29,6 +29,5 @@ public class IntersectionSphere
 		tryIntersect(new SphereMaths(new Point(0, 0, -2), 1), new Ray(new Point(0, 0, 0), new Point(0, 0, -1), true), new Point(0, 0, -1));
 		tryIntersect(new SphereMaths(new Point(0, 0, 2), 1), new Ray(new Point(0, 0, 0), new Point(0, 0, -1), true), null);
 		tryIntersect(new SphereMaths(new Point(-1, 0, -2), 1), new Ray(new Point(0, 0, 0), new Point(0, 0, -1), true), new Point(0, 0, -2));
-		tryIntersect(new SphereMaths(new Point(2, 0, 0), 1), new Ray(new Point(0, 0, 0), new Point(1.4, 0.2, 0.4), true), new Point(1.4, 0.2, 0.4));
 	}
 }
