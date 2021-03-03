d557ceed3ff42a5231b99e179c08a910d9c34fd8
DRUM0ND <baptiste.clochard@etu.unicaen.fr>
2021-03-03 22:31:58 +0100

    renommage de ExempleImageWriter en ImageWriter

---
+++ dev/src/render/ImageWriter.java
@@ -0,0 +1,89 @@
+package render;
+
+import java.util.ArrayList;
+
+import geometry.shapes.*;
+import geometry.*;
+
+import scene.*;
+import scene.MyScene;
+import scene.lights.*;
+import rayTracer.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.image.WritableImage;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+//from ww  w  . j  av  a2s  . c  om
+public class ImageWriter {
+
+
+
+public static void ImageWriterMain(int height, int width) {
+
+    Stage stage = new Stage();
+
+    WritableImage writableImage = new WritableImage(800,600);
+
+    PixelWriter pw = writableImage.getPixelWriter();
+
+    /*pw.setColor(2,2,Color.web("0x0000FF"));
+       pw.setColor(2,3,Color.web("0x0000FF"));
+       pw.setColor(3,2,Color.web("0x0000FF"));
+       pw.setColor(3,3,Color.web("0x0000FF"));/*Color.rgb(0,0,255)*/
+    /*Prend en argument un tableau de couleur*/
+
+    ImageView imageView = new ImageView();
+    imageView.setImage(writableImage);
+
+    Pane root = new Pane();
+    root.getChildren().add(imageView);
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.setTitle("");
+    stage.show();
+
+    /*pw.setColor(2,2,Color.web("0xFF0000"));
+       pw.setColor(3,2,Color.web("0xFF0000"));
+       pw.setColor(2,3,Color.web("0xFF0000"));
+       pw.setColor(3,3,Color.web("0xFF0000"));*/
+    //RayTracer rayTracer = new RayTracer(800,600);
+    //computeImage = rayTracer.computeImage();
+    //doImage(colorTab, pw);/*TODO trouver un moyen d'executer cette foction en récupérant le tableau ici*/
+
+    RayTracer r = new RayTracer(height, width);
+
+    Camera c = new Camera(); c.setFOV(100);
+    Light l = new LightBulb(Point.add(c.getPosition(), new Point(-1, 1, 0)), 1);
+
+    ArrayList<Shape> shapeList = new ArrayList<>();
+    shapeList.add(new SphereMaths(new Point(0, 0, -4), 1));
+    shapeList.add(new Triangle(new Point(-1,-1,-1.5),new Point(1,-1,-1.5),new Point(0,2,-1.5)));
+
+
+    MyScene s = new MyScene(c, l, shapeList, 0.5);
+
+
+    doImage(r.computeImage(s),pw);
+
+}
+
+public static void doImage(Color[][] colorTab, PixelWriter pw) {
+        System.out.println("colorTab.length = " + colorTab.length);
+        System.out.println("colorTab[0].length = " + colorTab[0].length);
+        int width = colorTab[0].length;
+        int height = colorTab.length;
+
+        for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                        //System.out.println(height);
+                        //System.out.println(String.format("%d %d",i,j));
+                        pw.setColor(j,i,colorTab[i][j]);
+                }
+        }
+        System.out.println("finish");
+
+}
+}
