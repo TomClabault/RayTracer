1c646380a6f06e65879793fe950a528a253e1182
DRUM0ND <baptiste.clochard@etu.unicaen.fr>
2021-03-04 15:10:59 +0100

    ajout de addObjectsToScene pour l'initialisation

--- dev/src/render/MainApp.java
+++ dev/src/render/MainApp.java
@@ -20,6 +20,16 @@ public class MainApp extends Application {
 
         ImageWriter imageWriter = new ImageWriter();
         imageWriter.ImageWriterMain(HEIGHT, WIDTH);
+    public MyScene addObjectsToScene() {
+
+        Camera c = new Camera(); c.setFOV(100);
+        Light l = new LightBulb(Point.add(c.getPosition(), new Point(-1, 1, 0)), 1);
+        ArrayList<Shape> shapeList = new ArrayList<>();
+        shapeList.add(new SphereMaths(new Point(0, 0, -4), 1));
+        shapeList.add(new Triangle(new Point(-1,-1,-1.5),new Point(1,-1,-1.5),new Point(0,2,-1.5)));
+        MyScene s = new MyScene(c, l, shapeList, 0.5);
+        return s;
+    }
     }
 
 }
