38b836db5a2e5a00440ba782d96ef44ddceaa5b4
DRUM0ND <baptiste.clochard@etu.unicaen.fr>
2021-03-08 16:30:56 +0100

    ajout de cameraTimer et updateCamera qui gèrent le déplacement de la cam

---
+++ dev/src/render/CameraTimer.java
@@ -0,0 +1,50 @@
+package render;
+
+import javafx.scene.input.KeyEvent;
+import javafx.scene.Scene;
+import javafx.event.EventHandler;
+import scene.MyScene;
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+import maths.Point;
+
+public class CameraTimer extends AnimationTimer {
+
+    public static final Double DELTA_MOVE = 0.1;
+
+    Scene scene;
+    MyScene myScene;
+
+    public CameraTimer(Scene scene, MyScene myScene) {
+        this.scene = scene;
+        this.myScene = myScene;
+    }
+
+    public void handle(long nanoTime){
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {/*TODO code sale à revoir*/
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.UP) {
+                    upGlobalCamera();
+                    System.out.println("Camera up");
+                }else if (event.getCode() == KeyCode.DOWN) {
+                    downGlobalCamera();
+                    System.out.println("Camera down");
+                }
+            }
+        });
+
+    }
+
+    public void upGlobalCamera() {
+        this.myScene.getCamera().getPosition().setY(this.myScene.getCamera().getPosition().getY() + DELTA_MOVE);
+        //System.out.println(this.myScene.getCamera().getPosition());
+        //Point tempPoint = this.myScene.getCamera().getPosition();
+        //tempPoint.setY(tempPoint.getY() + DELTA_MOVE);
+        //this.myScene.getCamera().setPosition(tempPoint);
+    }
+
+    public void downGlobalCamera() {
+        this.myScene.getCamera().getPosition().setY(this.myScene.getCamera().getPosition().getY() - DELTA_MOVE);
+    }
+}
--- dev/src/render/UpdateCamera.java
+++ dev/src/render/UpdateCamera.java
@@ -1,15 +1,10 @@
 package render;
 
 import scene.MyScene;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import java.util.logging.*;
 
 public class UpdateCamera implements Runnable {
 
-    public static final Double DELTA_MOVE = 0.1;
     Scene scene;
     MyScene myScene;
 
@@ -20,27 +15,7 @@ public class UpdateCamera implements Runnable {
 
     @Override
     public void run() {
-        while(true) {
-            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {/*TODO code sale à revoir*/
-                @Override
-                public void handle(KeyEvent event) {
-                    if (event.getCode() == KeyCode.UP) {
-                        upGlobalCamera();
-                        System.out.println("Camera up");
-                    }else if (event.getCode() == KeyCode.DOWN) {
-                        downGlobalCamera();
-                        System.out.println("Camera down");
-                    }
-                }
-            });
-        }
-    }
-
-    public void upGlobalCamera() {
-        this.myScene.getCamera().getPosition().setY(this.myScene.getCamera().getPosition().getY() + DELTA_MOVE);
-    }
-
-    public void downGlobalCamera() {
-        this.myScene.getCamera().getPosition().setY(this.myScene.getCamera().getPosition().getY() - DELTA_MOVE);
+        CameraTimer cameraTimer = new CameraTimer(this.scene, this.myScene);
+        cameraTimer.start();
     }
 }
