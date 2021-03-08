eb0bfff5ea0444d8b0778db995c7ac284cace1f1
DRUM0ND <baptiste.clochard@etu.unicaen.fr>
2021-03-08 19:48:45 +0100

    ajout des mouvements de rotation de la caméra

--- dev/src/render/CameraTimer.java
+++ dev/src/render/CameraTimer.java
@@ -1,7 +1,9 @@
 package render;
 
+import maths.MatrixD;
 import scene.MyScene;
 import maths.Point;
+import maths.RotationMatrix;
 
 import javafx.scene.input.KeyEvent;
 import javafx.scene.Scene;
@@ -12,6 +14,12 @@ import javafx.scene.input.KeyCode;
 public class CameraTimer extends AnimationTimer {
 
     private static final Double DELTA_MOVE = 0.1;
+    private static final Double DELTA_ANGLE = 1.0;
+
+    private final RotationMatrix PLUS_Y_ANGLE = new RotationMatrix(1, DELTA_ANGLE);
+    private final RotationMatrix MINUS_Y_ANGLE = new RotationMatrix(1, - DELTA_ANGLE);
+    private final RotationMatrix PLUS_X_ANGLE = new RotationMatrix(0, DELTA_ANGLE);
+    private final RotationMatrix MINUS_X_ANGLE = new RotationMatrix(0, - DELTA_ANGLE);
 
     private Scene scene;
     private MyScene myScene;
@@ -25,30 +33,57 @@ public class CameraTimer extends AnimationTimer {
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {/*TODO code sale à revoir*/
             @Override
             public void handle(KeyEvent event) {
-                if (event.getCode() == KeyCode.UP) {
-                    upGlobalCamera();
-                    System.out.println("Camera up");
-                }else if (event.getCode() == KeyCode.DOWN) {
-                    downGlobalCamera();
-                    System.out.println("Camera down");
+                if (event.getCode() == KeyCode.E) {
+                    upCamera();
+                    //System.out.println("Camera up");
+                }else if (event.getCode() == KeyCode.A) {
+                    downCamera();
+                    //System.out.println("Camera down");
+                } else if (event.getCode() == KeyCode.UP) {
+                    turnUpCamera();
+                } else if (event.getCode() == KeyCode.DOWN) {
+                    turnDownCamera();
+                } else if (event.getCode() == KeyCode.RIGHT) {
+                    turnRightCamera();
+                } else if (event.getCode() == KeyCode.LEFT) {
+                    turnLeftCamera();
                 }
             }
         });
 
     }
     /*TODO zqsd change la position, fleche change l'orientation de la caméra, space et shift pour changer la hauteur*/
-    public void upGlobalCamera() {
+    public void upCamera() {
         Point new_position = Point.add(this.myScene.getCamera().getPosition(),new Point(0, DELTA_MOVE,0));
         Point new_direction = Point.add(this.myScene.getCamera().getDirection(),new Point(0, DELTA_MOVE,0));
         this.myScene.getCamera().setPosition(new_position);
         this.myScene.getCamera().setDirection(new_direction);
     }
 
-    public void downGlobalCamera() {
+    public void downCamera() {
         Point new_position = Point.add(this.myScene.getCamera().getPosition(),new Point(0,- DELTA_MOVE,0));
         Point new_direction = Point.add(this.myScene.getCamera().getDirection(),new Point(0,- DELTA_MOVE,0));
         this.myScene.getCamera().setPosition(new_position);
         this.myScene.getCamera().setDirection(new_direction);
+    }
 
+    public void turnUpCamera() {/*0 pour l'axe x, 1 pour y et 2 pour z*/
+        Point new_direction = MatrixD.mulPoint(this.myScene.getCamera().getDirection(), PLUS_X_ANGLE);
+        this.myScene.getCamera().setDirection(new_direction);
+    }
+
+    public void turnDownCamera() {/*0 pour l'axe x, 1 pour y et 2 pour z*/
+        Point new_direction = MatrixD.mulPoint(this.myScene.getCamera().getDirection(), MINUS_X_ANGLE);
+        this.myScene.getCamera().setDirection(new_direction);
+    }
+
+    public void turnRightCamera() {/*0 pour l'axe x, 1 pour y et 2 pour z*/
+        Point new_direction = MatrixD.mulPoint(this.myScene.getCamera().getDirection(), MINUS_Y_ANGLE);
+        this.myScene.getCamera().setDirection(new_direction);
+    }
+
+    public void turnLeftCamera() {/*0 pour l'axe x, 1 pour y et 2 pour z*/
+        Point new_direction = MatrixD.mulPoint(this.myScene.getCamera().getDirection(), PLUS_Y_ANGLE);
+        this.myScene.getCamera().setDirection(new_direction);
     }
 }
