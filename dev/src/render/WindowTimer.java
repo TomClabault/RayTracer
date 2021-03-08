23ba5af7596dd8047c4cd96439f7878b5d3368d9
DRUM0ND <baptiste.clochard@etu.unicaen.fr>
2021-03-08 20:36:19 +0100

    ajout du compteur de fps

--- dev/src/render/WindowTimer.java
+++ dev/src/render/WindowTimer.java
@@ -1,7 +1,11 @@
 package render;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.animation.AnimationTimer;
 import javafx.scene.image.PixelWriter;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import scene.MyScene;
 import rayTracer.RayTracer;
@@ -11,17 +15,35 @@ public class WindowTimer extends AnimationTimer {
     private MyScene myScene;
     private PixelWriter pixelWriter;
     private RayTracer rayTracer;
+    private long oldFrameTime;
+    private Label fpsString;
 
     public WindowTimer(MyScene myScene, PixelWriter pixelWriter, RayTracer rayTracer) {
         this.myScene = myScene;
         this.pixelWriter = pixelWriter;
         this.rayTracer = rayTracer;
+
+        Pane root = new Pane();
+        Label fpsString = new Label("this is text");
+        fpsString.setStyle("-fx-font: 128 arial;");
+        this.fpsString = fpsString;
+        root.getChildren().add(fpsString);
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setTitle("Number of fps");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void handle(long nanoTime){
+    public void handle(long actualFrameTime){
+        long dif = actualFrameTime - oldFrameTime;
+        dif  = (long)1000000000.0 / dif;
+        this.oldFrameTime = actualFrameTime;
         ImageWriter.doImage(rayTracer.renderImage(this.myScene,8),this.pixelWriter);
+        fpsString.setText(String.format("FPS : %d", dif));
         //System.out.println("A Image is printed");
         //System.out.println(myScene.getCamera().getPosition());
 
     }
+
 }
