415a304fef1615cfa5dddeb2df4ffc001c744872
DRUM0ND <baptiste.clochard@etu.unicaen.fr>
2021-03-09 16:51:09 +0100

    ajout de la javadoc de CounerFPS

--- dev/src/render/CounterFPS.java
+++ dev/src/render/CounterFPS.java
@@ -3,19 +3,22 @@ package render;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 
+/**
+* Contient les composants javafx du compteur de FPS
+*/
 public class CounterFPS {
 
     private Pane pane;
 
+    /**
+    * @param fpsLabel le Label contenant le nombre de FPS
+    */
     public CounterFPS(Label fpsLabel) {
         Pane pane = new Pane();
         pane.getChildren().add(fpsLabel);
         this.pane = pane;
     }
 
-    /**
-     * @return the pane
-     */
     public Pane getPane() {
     	return pane;
     }
