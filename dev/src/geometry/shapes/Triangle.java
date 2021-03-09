f93d01d72bc39a2127cdfa70f5bee1c7ca2c73ce
Tom Clabault <Tomikaddle@orange.fr>
2021-03-09 10:26:33 +0100

    Ajout de doc pour le paramètre outNormalAtInter de Triangle.intersect

--- dev/src/geometry/shapes/Triangle.java
+++ dev/src/geometry/shapes/Triangle.java
@@ -74,7 +74,10 @@ public class Triangle implements Shape
 	 * Calcule l'intersection du triangle représenté par cette instance avec un rayon passé en paramètre. Cette méthode ne cherche l'intersection que dans la direction du rayon (c'est à dire pas "derrière" le rayon / derrière la caméra).
 	 * 
 	 * @param ray Le rayon avec lequel chercher une intersection
-	 * 
+	 * @param outNormalAtInter 	Ce vecteur reçevra la normale du triangle si un point d'intersection avec le rayon est trouvé. 
+	 * 							Si aucun point d'intersection n'est trouvé, ce vecteur reste inchangé. 
+	 * 							De même, si outNormalAtInter vaut null à l'appel de la méthode, le vecteur eestera inchangé et la normale du triangle ne sera pas tockée.
+	 *  
 	 * @return Le point d'intersection du rayon et du triangle. Null s'il n'y a pas d'intersection
 	 */
 	@Override
