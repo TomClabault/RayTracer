64003d79969f0fb16c033070fb331fae0e3ab60e
Thibaut Letourneur <tiblet24@gmail.com>
2021-03-10 21:09:27 +0100

    suppression fichier origin_master et ajout classe PovParser

---
+++ dev/src/povParser/PovParser.java
@@ -0,0 +1,69 @@
+package povParser;
+import java.io.*;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class PovParser
+{
+    private String pathName;
+    private final char NEW_LINE = '\n';
+    private final String SINGLE_LINE_COMMENT = "//";
+    private final String START_MULTILINE_COMMENT = "/*";
+    private final String END_MULTILINE_COMMENT = "*/";
+
+    public PovParser(String pathName)
+    {
+        this.pathName = pathName;
+    }
+
+    public File removeComments()
+    {
+        File povFile = new File("src/povParser/test2.pov");
+        File originalFile = new File(this.pathName);
+        try
+        {
+            Scanner oldFileScanner = new Scanner(originalFile);
+            BufferedWriter povFileWriter = new BufferedWriter(new FileWriter(povFile));
+            while (oldFileScanner.hasNextLine())
+            {
+                String currentLine = oldFileScanner.nextLine();
+                currentLine = currentLine.replaceAll("//.*", "");
+                currentLine = currentLine.replaceAll("/\\*.*\\*/", "");
+                if(! currentLine.isBlank())
+                    povFileWriter.write(currentLine);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        return povFile;
+    }
+
+    public static void printFile(File file)
+    {
+        try
+        {
+            Scanner povReader = new Scanner(file);
+            while (povReader.hasNextLine())
+            {
+                String currentLine = povReader.nextLine();
+                System.out.println(currentLine);
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args)
+    {
+        final String testFile = "src/povParser/temp.pov";
+        PovParser povFile= new PovParser(testFile);
+        File noComments = povFile.removeComments();
+    }
+}
