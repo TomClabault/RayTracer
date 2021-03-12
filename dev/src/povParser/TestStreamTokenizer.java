b030f98ef8cc66afa7e633b34f7ebdf347ee982f
Thibaut Letourneur <tiblet24@gmail.com>
2021-03-12 11:59:55 +0100

    avancée dans les tests de parsing

--- dev/src/povParser/TestStreamTokenizer.java
+++ dev/src/povParser/TestStreamTokenizer.java
@@ -1,15 +1,23 @@
 package povParser;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestStreamTokenizer
 {
+    ArrayList<String> figureList;
     private String pathToPovFile;
     private final char NEW_LINE = '\n';
     private final String SINGLE_LINE_COMMENT = "//";
     private final String START_MULTILINE_COMMENT = "/*";
     private final String END_MULTILINE_COMMENT = "*/";
+
+    private final char START_CURLY_BRACKET = '{';
+    private final char END_CURLY_BRACKET = '}';
+
+    private final char START_CHEVRON = '<';
+    private final char END_CHEVRON = '>';
 
     public TestStreamTokenizer(String pathToPovFile)
     {
@@ -24,22 +32,26 @@ public class TestStreamTokenizer
         this.pathToPovFile = pathToPovFile;
     }
 
-    public File removeComments()
+    public void removeComments()
     {
-        File povFile = new File("src/povParser/test.pov");
-        File originalFile = new File(this.pathToPovFile);
+        FileWriter povFile = null;
         try
         {
+            povFile = new FileWriter("src/povParser/test.pov");
+            File originalFile = new File(this.pathToPovFile);
             Scanner oldFileScanner = new Scanner(originalFile);
-            BufferedWriter povFileWriter = new BufferedWriter(new FileWriter(povFile));
+            BufferedWriter povFileWriter = new BufferedWriter(povFile);
             while (oldFileScanner.hasNextLine())
             {
                 String currentLine = oldFileScanner.nextLine();
                 currentLine = currentLine.replaceAll("//.*", "");
                 currentLine = currentLine.replaceAll("/\\*.*\\*/", "");
-                if(! currentLine.isBlank())
+                if(! currentLine.isBlank()) {
                     povFileWriter.write(currentLine);
+                    povFileWriter.write("\n");
+                }
             }
+            povFileWriter.close();
         }
         catch (FileNotFoundException e)
         {
@@ -49,14 +61,52 @@ public class TestStreamTokenizer
         {
             System.err.println(e.getMessage());
         }
-        return povFile;
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+        }
     }
-
 
     public static void main(String[] args)
     {
-        TestStreamTokenizer testStreamTokenizer = new TestStreamTokenizer("src/povParser/subsurface.pov");
-        File witoutComments = testStreamTokenizer.removeComments();
-        System.out.println(FileUtility.isFileEmpty(witoutComments.getPath()));
+        String pathToTestFile = "src/povParser/test.pov";
+        TestStreamTokenizer testFile = new TestStreamTokenizer("src/povParser/subsurface.pov");
+        testFile.removeComments();
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(pathToTestFile));
+            BufferedReader fileReader = new BufferedReader(inputStreamReader);
+
+            StreamTokenizer streamTokenizer = new StreamTokenizer(fileReader);
+            int currentToken = streamTokenizer.nextToken();
+            while(currentToken != StreamTokenizer.TT_EOF)
+            {
+                streamTokenizer.quoteChar(testFile.START_CURLY_BRACKET);
+                streamTokenizer.quoteChar(testFile.START_CHEVRON);
+
+                if (currentToken == StreamTokenizer.TT_WORD)
+                {
+                    System.out.println("word");
+                }
+                else if (currentToken == StreamTokenizer.TT_NUMBER)
+                {
+                    System.out.println("number");
+                }
+                else
+                {
+                    System.out.println(streamTokenizer.sval);
+                }
+                currentToken = streamTokenizer.nextToken();
+            }
+        }
+
+        catch (FileNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }
+
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
