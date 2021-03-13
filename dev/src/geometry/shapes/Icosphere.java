c113654120f65d51bd4f96b103dfdeb2bc93cbc0
Willie Tanzil <willie.tanzil99@gmail.com>
2021-03-13 14:03:41 +0100

    ajout classe icosphere contenant des points

---
+++ dev/src/geometry/shapes/Icosphere.java
@@ -0,0 +1,59 @@
+package geometry.shapes;
+
+import geometry.materials.Material;
+import geometry.materials.MatteMaterial;
+import geometry.shapes.Triangle;
+import javafx.scene.paint.Color;
+import maths.Point;
+import geometry.Shape;
+import geometry.ShapeTriangle;
+import maths.Ray;
+import maths.Vector;
+
+import java.util.ArrayList;
+import java.lang.Math;
+
+
+
+
+public class Icosphere extends ShapeTriangle
+{
+    /*
+
+    BottomPoint = (0.5, 0.25, 0.5)
+    Centre = (0.5, 0.75, 0.5)
+    TopPoint = (0.5, 1.25, 0.5)
+
+    Atop (0,1,0)
+    Btop (1,1,0)
+    Ctop (1.25,1,0.75)
+    Dtop (0.75,1,1.25)
+    Etop (0,1,1)
+
+    rotated pentagon
+    Arot (0.5,0.5,0)
+    Brot (1,0.5,0.75/2)
+    Crot (1,0.5,1)
+    Drot (0.75/2,0.5,1)
+    Erot (0,0.5,0.5)
+
+
+
+
+
+
+     */
+
+
+    protected Point BottomPoint,Centre,TopPoint,Atop,Btop,Ctop,Dtop,Etop,Arot,Brot,Crot,Drot,Erot;
+
+
+
+
+
+
+
+
+
+
+}
