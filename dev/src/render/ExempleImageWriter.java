c47ddaf7b100dc507bf024254c608f56832534cc
Willie Tanzil <willie.tanzil99@gmail.com>
2021-03-19 12:20:20 +0100

    icosphere ca marche, sauf la subdivision

--- dev/src/render/ExempleImageWriter.java
+++ dev/src/render/ExempleImageWriter.java
@@ -58,20 +58,22 @@ public class ExempleImageWriter extends Application
 		
 		RayTracer rayTracerInstance = new RayTracer(width, height);
 
-		Camera cameraRT = new Camera(new Point(-1, 1, -2), new Point(0, 0, -6));
+		Camera cameraRT = new Camera(new Point(-1, 1, 7), new Point(0, 0, -6));
 		cameraRT.setFOV(60);
-		Light l = new LightBulb(new Point(-1,0.75,-1), 1.25);
+		Light l = new LightBulb(new Point(-1,1,7), 1.25);
 
 		ArrayList<Shape> shapeList = new ArrayList<>();
 		shapeList.add(new PlaneMaths(new Vector(0, 1, 0), new Point(0, -1, 0), new MatteMaterial(Color.rgb(128, 128, 128))));
 		
-		shapeList.add(new SphereMaths(new Point(0, 0, -6), 1, new MetallicMaterial(Color.rgb(240, 0, 0))));
-		shapeList.add(new SphereMaths(new Point(1.1, 0.5, -5.5), 0.2, new MetallicMaterial(Color.rgb(255, 211, 0))));
-		shapeList.add(new SphereMaths(new Point(-1.25, 1, -6.5), 0.2, new MetallicMaterial(Color.LIGHTSKYBLUE)));
-		shapeList.add(new SphereMaths(new Point(-1.5, -0.65, -5.5), 0.35, new MatteMaterial(Color.ORANGERED)));
-		shapeList.add(new SphereMaths(new Point(1.5, -0.65, -5), 0.35, new MirrorMaterial(0.75)));
+		//shapeList.add(new SphereMaths(new Point(0, 0, -6), 1, new MetallicMaterial(Color.rgb(240, 0, 0))));
+		//shapeList.add(new SphereMaths(new Point(1.1, 0.5, -5.5), 0.2, new MetallicMaterial(Color.rgb(255, 211, 0))));
+		//shapeList.add(new SphereMaths(new Point(-1.25, 1, -6.5), 0.2, new MetallicMaterial(Color.LIGHTSKYBLUE)));
+		//shapeList.add(new SphereMaths(new Point(-1.5, -0.65, -5.5), 0.35, new MatteMaterial(Color.ORANGERED)));
+		//shapeList.add(new SphereMaths(new Point(1.5, -0.65, -5), 0.35, new MirrorMaterial(0.75)));
 
-		shapeList.add(new Pyramide(new Point(0,-1,-5),0.2,0.1,new MetallicMaterial(Color.rgb(128, 128, 128))));
+		//shapeList.add(new Pyramide(new Point(0,-1,-5),0.2,0.1,new MetallicMaterial(Color.rgb(128, 128, 128))));
+
+		shapeList.add(new Icosphere(new Point(2,2,0),1,new MetallicMaterial(Color.RED)));
 		
 		
 		MyScene sceneRT = new MyScene(cameraRT, l, shapeList, Color.rgb(32, 32, 32), 0.55);
