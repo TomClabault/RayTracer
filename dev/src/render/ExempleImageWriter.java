9b45daf01b06df9c9702056e26a24b95e2f84639
Tom Clabault <Tomikaddle@orange.fr>
2021-03-20 09:09:47 +0100

    Ajout de l'explication de l'addition des composantes de phong

--- dev/src/render/ExempleImageWriter.java
+++ dev/src/render/ExempleImageWriter.java
@@ -67,7 +67,7 @@ public class ExempleImageWriter extends Application
 		ArrayList<Shape> shapeList = new ArrayList<>();
 		//shapeList.add(new PlaneMaths(new Vector(0, 1, 0), new Point(0, -1, 0), new MatteMaterial(Color.rgb(128, 128, 128))));
 		
-		shapeList.add(new SphereMaths(new Point(0, 0, -6), 1, new Material(Color.rgb(255, 0, 0), 0, 0, 0, 0.33, 512)));
+		shapeList.add(new SphereMaths(new Point(0, 0, -6), 1, new Material(Color.rgb(255, 0, 0), 0.4, 1, 0, 1, 256)));
 		//shapeList.add(new SphereMaths(new Point(0, 0, -6), 1, new MetallicMaterial(Color.RED)));
 		//shapeList.add(new SphereMaths(new Point(1.1, 0.5, -5.5), 0.2, new MetallicMaterial(Color.rgb(255, 211, 0))));
 		//shapeList.add(new SphereMaths(new Point(-1.25, 1, -6.5), 0.2, new MetallicMaterial(Color.LIGHTSKYBLUE)));
