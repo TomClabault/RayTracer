261d77538c44a3aef51450aaecc5f62701293b1e
Tom Clabault <Tomikaddle@orange.fr>
2021-03-26 16:31:55 +0100

    Correction d'une erreur dans GlassMaterial

--- dev/src/geometry/materials/GlassMaterial.java
+++ dev/src/geometry/materials/GlassMaterial.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 public class GlassMaterial extends Material{
 
     public GlassMaterial() {//1,474 /*indice de refraction du pirex selon wikipedia*/
-        super(Color.WHITE,0 , 0, 0, 0,  1,true, 1.474);
+        super(Color.WHITE, 0, 0, 0,  1,true, 1.474);
 
     }
 }
