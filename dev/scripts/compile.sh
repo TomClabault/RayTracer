9caad15937b8a6376488d36da30d10f4d37d3997
Thibaut Letourneur <tiblet24@gmail.com>
2021-03-29 10:00:18 +0200

    ajout script run.sh qui sert à exécuter le projet

--- dev/scripts/compile.sh
+++ dev/scripts/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ../
-mkdir lib
+[ -d lib ] || mkdir lib
 wget https://gluonhq.com/download/javafx-16-sdk-linux/ /lib/javafx.zip #peut mettre un certain temps à s'exécuter
 unzip lib/javafx.zip
 rm -rf lib/javafx.zip
