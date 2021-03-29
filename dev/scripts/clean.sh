30b3a26769f5ed5d32d8023f6c4d23227b701959
Thibaut Letourneur <tiblet24@gmail.com>
2021-03-29 10:23:17 +0200

    ajout fichier script clean.sh

---
+++ dev/scripts/clean.sh
@@ -0,0 +1,16 @@
+ #!/bin/sh
+
+ if [-d ../doc]
+ then
+    rm -rf ../doc
+fi
+
+if [-d ../lib]
+then
+    rm -rf ../lib
+fi
+
+if[-d ../build]
+then
+    rm -rf ../build
+fi
--- dev/scripts/makedoc.sh
+++ dev/scripts/makedoc.sh
@@ -1,4 +1,5 @@
 #!/bin/doc
 
+cd ..
 [-d doc || mkdir doc]
 javadoc -d /doc -sourcepath /src
