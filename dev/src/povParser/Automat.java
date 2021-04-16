d0b636e371e4c2fe9121f6bef6fbbcb86a25c784
Thibaut Letourneur <tiblet24@gmail.com>
2021-04-16 13:02:34 +0200

    ajout de javadoc

--- dev/src/povParser/Automat.java
+++ dev/src/povParser/Automat.java
@@ -12,7 +12,6 @@ public class Automat
 {
     /*TODO
         -ajout de la javadoc
-        -enlever les sysout
      */
 
     /*
@@ -26,7 +25,7 @@ public class Automat
     private EtatToken etatToken;
 
     /**
-     *instance de StreamTokenizer utilisée pour effectuer le parsing
+     * instance de StreamTokenizer utilisée pour effectuer le parsing
      */
     private StreamTokenizer streamTokenizer;
 
@@ -36,45 +35,80 @@ public class Automat
     private State currentState;
 
     /**
-     *
+     * jeton courant du StreamTokenizer, c'est un entier qui peut être cast en char pour récupérer des charactères (comme '<', '{')
      */
     private int currentToken;
 
+    /**
+     * Constructeur complet d'Automat
+     * @param streamTokenizer Une instance de classe StreamTokenizer qui va servir de base à l'automat
+     * @param currentState état courant de Figure
+     */
     public Automat(StreamTokenizer streamTokenizer, State currentState)
     {
         this.streamTokenizer = streamTokenizer;
         this.currentState = currentState;
     }
 
+    /**
+     * Constructeur d'Automat qui omet l'état courant et qui l'initialise à OUTSIDE
+     * @param streamTokenizer streamTokenizer servant à parser le fichier
+     */
     public Automat(StreamTokenizer streamTokenizer)
     {
         this(streamTokenizer, State.OUTSIDE);
     }
 
+    /**
+     * Mutateur servant à fixer la valeur de l'état courant (qui est une sous classe de EtatToken) qui va être prochainement parsé
+     * @param etatToken Classe d'état qui implémente EtatToken
+     */
     public void setState(EtatToken etatToken)
     {
         this.etatToken = etatToken;
     }
+
+    /**
+     * Méthode servant à lancer le parsing de la figure courante, c'est à dire d'appeler la méthode action de la classe d'état courante
+     * @return Elle retourne l'objet résultant du parsing, ensuite casté en une figure, lumière etc. (polymorphisme)
+     */
     public Object action()
     {
         return etatToken.action(this);
     }
 
+    /**
+     * Accesseur donnant la valeur du streamTokenizer
+     * @return Retourne l'attribut streamTokenizer
+     */
     public StreamTokenizer getStreamTokenizer()
     {
         return this.streamTokenizer;
     }
 
+    /**
+     * Cette méthode teste si le type du streamTokenizer est un mot
+     * @return retourne true si c'est un mot, false sinon
+     */
     public boolean isCurrentTokenAWord()
     {
         return streamTokenizer.ttype == StreamTokenizer.TT_WORD;
     }
 
+    /**
+     * Cette méthode teste si un mot donné est égal au jeton courant du streamTokenizer
+     * @param word le mot à tester
+     * @return retourne true si le mot passé en argument est égal au jeton courant, false sinon
+     */
     public boolean currentWord(String word)
     {
         return streamTokenizer.sval.equals(word);
     }
 
+    /**
+     * Méthode appelant simplement le prochain jeton dans le fichier
+     * @return retourne l'entier correspondant au jeton
+     */
     public int callNextToken()
     {
         try {
@@ -87,16 +121,28 @@ public class Automat
         }
     }
 
+    /**
+     * Mutateur fixant la valeur de l'attribut streamTokenizer
+     * @param st
+     */
     public void setStreamTokenizer(StreamTokenizer st)
     {
         this.streamTokenizer = st;
     }
 
+    /**
+     * Cette méthode renvoit la valeur du nombre parsée
+     * @return nombre parsé
+     */
     public double getNumberValue()
     {
         return this.streamTokenizer.nval;
     }
 
+    /**
+     * Cette méthode teste si le jeton courant est une figure valide
+     * @return true si la figure est valide, false sinon
+     */
     public boolean isValidState()
     {
         try {
@@ -118,16 +164,28 @@ public class Automat
         return false;
     }
 
+    /**
+     * Teste si on a atteint la fin du fichier
+     * @return true si on a fini le parsing, false sinon
+     */
     public boolean isFinished()
     {
         return (this.streamTokenizer.ttype == StreamTokenizer.TT_EOF);
     }
 
+    /**
+     * Acceseur donnant la valeur de l'attribut currentToken
+     * @return
+     */
     public int getCurrentToken()
     {
         return this.currentToken;
     }
 
+    /**
+     * Méthode donnant l'état courant (figure, light...) que l'on va parser
+     * @return une constante de l'énumération State
+     */
     public State getState()
     {
         if(this.streamTokenizer.sval.equals("sphere"))
