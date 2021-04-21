808b6f44796ff9df16868dbd9acf0bef8a5305da
Tom Clabault <Tomikaddle@orange.fr>
2021-04-21 11:08:56 +0200

    Ajout de lignes d'espacement dans la doc de RayTracerSettings

--- dev/src/rayTracer/RayTracerSettings.java
+++ dev/src/rayTracer/RayTracerSettings.java
@@ -3,22 +3,22 @@ package rayTracer;
 /**
  * 
  * Représente les réglages de rendu qui seront utilisés par le ray tracer. Ces réglages sont dynamiques et peuvent être modifiés pendant le rendu.
- * Le ray Tracer adaptera alors son rendu en conséquence
+ * Le ray Tracer adaptera alors son rendu en conséquence<br>
  * 
- * Paramètres:
- * nbCore: Détermine le nombre de thread sur lequel sera effectué le rendu des images
- * recursionDepth: Gère le nombre d'appel récursif maximum que peut faire le ray tracer. Détermine par exemple le nombre consécutifs de reflets que l'on peut observer dans deux surfaces se réfléchissant l'une l'autre
- * antialiasingSampling: Détermine le nombre de sous-pixel calculé pour chaque pixel afin de réduire l'aliasing (effet d'escalier) de l'image
- * blurryReflectionsSampleCount: Combien d'échantillons seront utilisés pour calculer les réflexion flous de certains matériaux 
+ * Paramètres:<br>
+ * nbCore: Détermine le nombre de thread sur lequel sera effectué le rendu des images<br>
+ * recursionDepth: Gère le nombre d'appel récursif maximum que peut faire le ray tracer. Détermine par exemple le nombre consécutifs de reflets que l'on peut observer dans deux surfaces se réfléchissant l'une l'autre<br>
+ * antialiasingSampling: Détermine le nombre de sous-pixel calculé pour chaque pixel afin de réduire l'aliasing (effet d'escalier) de l'image<br>
+ * blurryReflectionsSampleCount: Combien d'échantillons seront utilisés pour calculer les réflexion flous de certains matériaux <br>
  *	
- * enableAmbient: Permet d'activer ou de désactiver l'effet de lumière ambiante de l'ombrage de Phong
- * enableDiffuse: Permet d'activer ou de désactiver l'effet de lumière diffuse de l'ombrage de Phong
- * enableReflections: Permet d'activer ou de désactiver les réflexions des matériaux du rendu
- * enableBlurryReflections: Permet d'activer / désactiver les réflexions floues des matériaux
- * enableRefractions: Permet d'activer ou de désactiver les réfractions des matériaux du rendu
- * enableSpecular: Permet d'activer ou de désactiver l'effet de lumière spéculaire de l'ombrage de Phong
- * enableFresnel: Permet d'activer ou de désactiver les reflets aux bords des objets réfractifs
- * enableAntialiasing: Permet d'activer / désactiver l'antialiasing lors du rendu
+ * enableAmbient: Permet d'activer ou de désactiver l'effet de lumière ambiante de l'ombrage de Phong<br>
+ * enableDiffuse: Permet d'activer ou de désactiver l'effet de lumière diffuse de l'ombrage de Phong<br>
+ * enableReflections: Permet d'activer ou de désactiver les réflexions des matériaux du rendu<br>
+ * enableBlurryReflections: Permet d'activer / désactiver les réflexions floues des matériaux<br>
+ * enableRefractions: Permet d'activer ou de désactiver les réfractions des matériaux du rendu<br>
+ * enableSpecular: Permet d'activer ou de désactiver l'effet de lumière spéculaire de l'ombrage de Phong<br>
+ * enableFresnel: Permet d'activer ou de désactiver les reflets aux bords des objets réfractifs<br>
+ * enableAntialiasing: Permet d'activer / désactiver l'antialiasing lors du rendu<br>
  */
 public class RayTracerSettings 
 {
